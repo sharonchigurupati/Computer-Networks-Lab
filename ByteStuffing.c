734daba6d7a3234b3228118a222c9c64c0a4de29
sharonchigurupati <51990804+sharonchigurupati@users.noreply.github.com>
2020-09-17 11:53:11 +0530

    Add files via upload

---
+++ ByteStuffing.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+char header[10];
+char trailer[10];
+char ch[100];
+char add[10];
+char out[100];
+char esc[] = "esc";
+int k = 0,l,i,j;
+printf("Enter the message :");
+scanf("%s", ch);
+printf("Enter starting delimiter string or character:");
+scanf("%s", header);
+printf("Enter ending delimiter string or character:");
+scanf("%s", trailer);
+printf("Enter character or string that is to be stuffed:");
+scanf("%s", add);
+
+for (i = 0; i <strlen(header); i++) {
+out[k] = header[i];
+k++;
+}
+
+for (i = 0; i < strlen(ch); i++) {
+int count = 0;
+for (j = i, l = 0; j < (i + strlen(header)); j++,l++) {
+if (ch[j] == header[l]) {
+count++;
+} else {
+break;
+}
+}
+int m;
+if (count == strlen(header)) {
+for (m = 0; m < strlen(add); m++) {
+out[k] = add[m];
+k++;
+}
+}
+count = 0;
+for (j = i, l = 0; j < (i + strlen(trailer)); j++,l++) {
+                        if (ch[j] == trailer[l]) {
+                                count++;
+                        } else {
+                                break;
+                        }
+                }
+                if (count == strlen(trailer)) {
+                        for (m = 0; m < strlen(add); m++) {
+                                out[k] = add[m];
+                                k++;
+                        }
+                }
+count = 0;
+for (j = i, l = 0; j < (i + strlen(esc)); j++,l++) {
+                        if (ch[j] == esc[l]) {
+                                count++;
+                        } else {
+                                break;
+                        }
+                }
+                if (count == strlen(esc)) {
+                        for (m = 0; m < strlen(add); m++) {
+                                out[k] = add[m];
+                                k++;
+                        }
+                }
+
+out[k] = ch[i];
+k++;
+}
+
+for (i = 0; i <strlen(trailer); i++) {
+                out[k] = trailer[i];
+                k++;
+        }
+
+out[k] = '\0';
+
+printf("\nAfter Stuffing: %s\n", out);
+return 0;
+}
+
